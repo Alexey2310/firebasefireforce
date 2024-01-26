69881e2a51083d8879a97c9f97518afe6b67a285
Alex2310 <alexnozdrin23@gmail.com>
2024-01-26 21:41:30 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get startedd by editing&nbsp;
+          Get startedd l;by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
